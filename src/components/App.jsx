a2fb4898aa7750d894ef257ae9472cf3d4932ea6
DmytroValMan <dmitri.m2011@gmail.com>
2025-05-01 16:32:10 +0300

    react hw-4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,105 @@
-import { useState, useEffect } from "react";
-import { nanoid } from "nanoid";
+import { useState } from "react";
+import axios from "axios";
 
-import ContactForm from "./ContactForm/ContactForm";
-import SearchBox from "./SearchBox/SearchBox";
-import ContactList from "./ContactList/ContactList";
-
-const contactsList = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+import SearchBar from "./SearchBar/SearchBar";
+import ImageGallery from "./ImageGallery/ImageGallery";
+import LoadMoreBtn from "./LoadMoreBtn/LoadMoreBtn";
+import ErrorMessage from "./ErrorMessage/ErrorMessage";
+import Loader from "./Loader/Loader";
+import ImageModal from "./ImageModal/ImageModal";
 
 const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const contactsListJson = localStorage.getItem("contactsList");
-    if (contactsListJson !== null) {
-      return JSON.parse(contactsListJson);
+  const [images, setImages] = useState([]);
+  const [clicks, setclicks] = useState(1);
+  const [request, setRequest] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleSearch = async (newRequest, isNewRequest = false) => {
+    setError("");
+    setRequest(newRequest);
+
+    if (isNewRequest) {
+      setImages([]);
+      setclicks(1);
     }
-    return [];
-  });
-  const [searchField, setSearchField] = useState("");
 
-  useEffect(() => {
-    localStorage.setItem("contactsList", JSON.stringify(contacts));
-  }, [contacts]);
+    if (!newRequest) {
+      setImages([]);
+      return;
+    } else {
+      const accessKey = "HQG4djIhGZUrD70kI9DGgoTyajMzR2prd6xK5ZdenKE";
+      axios.defaults.baseURL = "https://api.unsplash.com/";
 
-  const handleContacts = (newContact) => {
-    newContact.id = nanoid();
-    setContacts((actualContacts) => [...actualContacts, newContact]);
+      const pageNumber = isNewRequest ? 1 : clicks;
+      const searchParams = {
+        query: newRequest,
+        per_page: 12,
+        orientation: "landscape",
+        page: pageNumber,
+      };
+
+      try {
+        setLoading(true);
+        const response = await axios.get("/search/photos", {
+          params: searchParams,
+          headers: { Authorization: `Client-ID ${accessKey}` },
+        });
+        isNewRequest
+          ? setImages(response.data.results)
+          : setImages((prevImages) => [
+              ...prevImages,
+              ...response.data.results,
+            ]);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
   };
 
-  const seachedContacts = contacts.filter((contact) => {
-    return contact.name.toLowerCase().includes(searchField.toLowerCase());
-  });
+  const handleClicks = () => {
+    setclicks(clicks + 1);
+    handleSearch(request);
+  };
 
-  const deleteContact = (contactId) => {
-    setContacts(() => contacts.filter((contact) => contact.id !== contactId));
+  const openModal = (image) => {
+    setSelectedImage({
+      created: image.created_at,
+      likes: image.likes,
+      description: image.description,
+      user: image.user.name,
+      url: image.urls.regular,
+    });
+    document.body.style.overflow = "hidden";
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedImage(null);
+    document.body.style.overflow = "auto";
   };
 
   return (
     <div>
-      <h1>Phonebook</h1>
-      <ContactForm onAddContact={handleContacts} />
-      <SearchBox value={searchField} onSearch={setSearchField} />
-      <ContactList contacts={seachedContacts} onDelete={deleteContact} />
+      <SearchBar onSearch={handleSearch} />
+
+      {error ? (
+        <ErrorMessage errorText={error} />
+      ) : (
+        <ImageGallery images={images} onImageClick={openModal} />
+      )}
+      {loading && <Loader />}
+      {images.length > 0 && !error && <LoadMoreBtn onClick={handleClicks} />}
+      <ImageModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        image={selectedImage}
+      />
     </div>
   );
 };
